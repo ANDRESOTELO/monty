2a23403bf60d585ac8c25e0a52c8b1e693b890d6
ANDRESOTELO <andresotelo2020@gmail.com>
2020-12-22 16:40:45 +0000

    Validate if stack have only 2 elements or less

--- swap_function.c
+++ swap_function.c
@@ -11,7 +11,7 @@ void swap_function(stack_t **head, unsigned int line_num)
 	stack_t *temp = variables.head;
 	(void)head;
 
-	if (!temp->next)
+	if (temp->next == NULL)
 	{
 		fprintf(stderr, "L%u: can't swap, stack too short\n", line_num);
 		want_to_be_free();
@@ -21,9 +21,18 @@ void swap_function(stack_t **head, unsigned int line_num)
 	{
 		variables.head = variables.head->next;
 		variables.head->prev = NULL;
-		temp->next = variables.head->next;
-		variables.head->next->prev = temp;
-		variables.head->next = temp;
-		temp->prev = variables.head;
+		if (variables.head->next == NULL)
+		{
+			variables.head->next = temp;
+			temp->next = NULL;
+			temp->prev = variables.head;
+		}
+		else
+		{
+			temp->next = variables.head->next;
+			variables.head->next->prev = temp;
+			variables.head->next = temp;
+			temp->prev = variables.head;
+		}
 	}
 }
