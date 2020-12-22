d021e90f4d6c6e655390b50503ed1add5625fa75
ANDRESOTELO <andresotelo2020@gmail.com>
2020-12-22 02:38:54 +0000

    Struct functions

---
+++ functions.c
@@ -0,0 +1,39 @@
+#include "monty.h"
+
+/**
+ *get_function - Get function from struct
+ *@line_num: Line number
+ */
+
+void get_function(unsigned int line_num)
+{
+	char *opcode = NULL;
+	int i = 0;
+
+	instruction_t op[] = {
+		{"push", push_stack},
+		{"pall", pall_function},
+		{NULL, NULL},
+	};
+
+	opcode = strtok(variables.buffer, " ");
+
+	if (opcode != NULL)
+	{
+		while (op[i].opcode)
+		{
+			if (strcmp(op[i].opcode, opcode) == 0)
+			{
+				op[i].f(&variables.head, line_num);
+				break;
+			}
+			i++;
+		}
+	}
+	if (op[i].opcode == NULL)
+	{
+		fprintf(stderr, "L%u: unknown instruction %s\n", line_num, opcode);
+		want_to_be_free();
+		exit(EXIT_FAILURE);
+	}
+}
