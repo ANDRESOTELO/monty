d3c227467a38327c5456c83a9c7808ef3dbe9779
ANDRESOTELO <andresotelo2020@gmail.com>
2020-12-23 22:22:22 +0000

    Correct betty style

--- functions.c
+++ functions.c
@@ -11,26 +11,14 @@ void get_function(unsigned int line_num)
 	int i = 0;
 
 	instruction_t op[] = {
-		{"push", push_stack},
-		{"pall", pall_function},
-		{"pint", pint_function},
-		{"pop", pop_function},
-		{"swap", swap_function},
-		{"add", add_function},
-		{"nop", nop_function},
-		{"sub", sub_function},
-		{"div", div_function},
-		{"mul", mul_function},
-		{"mod", mod_function},
-		{"pchar", pchar_function},
-		{"pstr", pstr_function},
-		{"rotl", rotl_function},
-		{"rotr", rotr_function},
+		{"push", push_stack}, {"pall", pall_function}, {"pint", pint_function},
+		{"pop", pop_function}, {"swap", swap_function}, {"add", add_function},
+		{"nop", nop_function}, {"sub", sub_function}, {"div", div_function},
+		{"mul", mul_function}, {"mod", mod_function}, {"pchar", pchar_function},
+		{"pstr", pstr_function}, {"rotl", rotl_function}, {"rotr", rotr_function},
 		{NULL, NULL},
 	};
-
 	opcode = strtok(variables.buffer, " ");
-
 	if (opcode != NULL && opcode[0] != '#')
 	{
 		while (op[i].opcode)
@@ -46,7 +34,6 @@ void get_function(unsigned int line_num)
 	if (op[i].opcode == NULL)
 	{
 		fprintf(stderr, "L%u: unknown instruction %s\n", line_num, opcode);
-		want_to_be_free();
-		exit(EXIT_FAILURE);
+		want_to_be_free(), exit(EXIT_FAILURE);
 	}
 }
